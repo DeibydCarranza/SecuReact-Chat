c47957dc52231a82e898b4eb2a0ad40682052723
DiegoAndresRa <diego.unam.15@gmail.com>
2024-04-01 00:30:47 -0600

    modify: index.js was modify to test Input implementation

--- Backend/index.js
+++ Backend/index.js
@@ -7,9 +7,12 @@ const server = createServer(app) // creando un servidor HTTP con el manejador de
 //  creando servidor Socket.io
 const io = new SocketServer(server) 
 
-io.on("connection", 
-    (socket)=>{
-        console.log("Client connected on port 5173")
+io.on("connection", (socket)=>{
+        console.log("\n-----------------------")
+        console.log(`Client connected ID: ${socket.id}`)
+        socket.on("Message",(messageIncomming)=>{
+            console.log(`From: ${messageIncomming.from}\n\r${messageIncomming.content}\n\r${messageIncomming.time}\n\r-----------------------\n\n`)
+        })
     })
 server.listen(4000) 
 console.log("Usuario Conectado",4000)
